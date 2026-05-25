2d1bea58c66070fcf12eab6c68714a1ab1210c66
Евгений Колпаков <pompei@mail.ru>
2026-05-25 19:09:47 +0500

    Added triangle on OpenGL

--- main.cpp
+++ main.cpp
@@ -1,18 +1,82 @@
+#include <SDL2/SDL.h>
+#include <GL/gl.h>
+
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+namespace {
+  constexpr int kWindowWidth = 800;
+  constexpr int kWindowHeight = 600;
 
-int main() {
-  // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+  void drawTriangle() {
+    glClearColor(0.08F, 0.10F, 0.14F, 1.0F);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-  const auto lang = "C++";
-  std::cout << "Hello and welcome to " << lang << "!\n";
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.0F, 0.20F, 0.25F);
+    glVertex2f(-0.65F, -0.55F);
+    glColor3f(0.15F, 0.85F, 0.35F);
+    glVertex2f(0.65F, -0.55F);
+    glColor3f(0.25F, 0.45F, 1.0F);
+    glVertex2f(0.0F, 0.65F);
+    glEnd();
+  }
+} // namespace
 
-  for (int i = 1; i <= 5; i++) {
-    // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    std::cout << "i = " << i << std::endl;
+int main(int, char **) {
+  if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+    std::cerr << "SDL_Init failed: " << SDL_GetError() << '\n';
+    return 1;
   }
 
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+  SDL_Window *window = SDL_CreateWindow(
+    "OpenGL Triangle",
+    SDL_WINDOWPOS_CENTERED,
+    SDL_WINDOWPOS_CENTERED,
+    kWindowWidth,
+    kWindowHeight,
+    SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+
+  if (window == nullptr) {
+    std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << '\n';
+    SDL_Quit();
+    return 1;
+  }
+
+  SDL_GLContext context = SDL_GL_CreateContext(window);
+  if (context == nullptr) {
+    std::cerr << "SDL_GL_CreateContext failed: " << SDL_GetError() << '\n';
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 1;
+  }
+
+  SDL_GL_SetSwapInterval(1);
+
+  bool running = true;
+  while (running) {
+    SDL_Event event{};
+    while (SDL_PollEvent(&event) != 0) {
+      if (event.type == SDL_QUIT) {
+        running = false;
+      } else if (event.type == SDL_KEYDOWN &&
+                 event.key.keysym.sym == SDLK_ESCAPE) {
+        running = false;
+      } else if (event.type == SDL_WINDOWEVENT &&
+                 event.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+        glViewport(0, 0, event.window.data1, event.window.data2);
+      }
+    }
+
+    drawTriangle();
+    SDL_GL_SwapWindow(window);
+  }
+
+  SDL_GL_DeleteContext(context);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
   return 0;
-  // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
