c594128c3352f404ad804b5322a6cd71dfcf0477
Евгений Колпаков <pompei@mail.ru>
2026-05-30 20:30:31 +0500

    Move out of class methods implementations in World_01

--- src/world_01.cpp
+++ src/world_01.cpp
@@ -32,68 +32,9 @@ struct world::World_01::Impl
   std::vector<atom::Atom> atoms_;
   const float             R = 0.5F;
 
-  void initShapes(const scene::ShapeGroup &atoms, const std::vector<scene::MaterialParams> &materials)
-  {
-    std::random_device             rd;
-    std::mt19937                   gen(rd());
-    std::uniform_real_distribution dist(0.0F, 1.0F);
+  void initShapes(const scene::ShapeGroup &atoms, const std::vector<scene::MaterialParams> &materials);
 
-    const std::size_t atomsCount = atoms.shapes.size();
-
-    positions_.resize(atomsCount);
-    moveX_.resize(atomsCount);
-    moveY_.resize(atomsCount);
-    angle_.resize(atomsCount);
-    atoms_.resize(atomsCount);
-    velocity_.resize(atomsCount);
-
-    for (std::size_t i = 0; const scene::Shape &shape : atoms.shapes)
-    {
-      const scene::MaterialParams material = materials[shape.materialIndex];
-
-      atoms_[i]     = material.atom;
-      positions_[i] = shape.offset;
-
-      glm::vec3 moveX = normalize(glm::vec3(dist(gen) - 0.5F, dist(gen) - 0.5F, dist(gen) - 0.5F));
-      glm::vec3 moveY = normalize(glm::vec3(dist(gen) - 0.5F, dist(gen) - 0.5F, dist(gen) - 0.5F));
-
-      {
-        glm::vec3 normal = glm::cross(moveX, moveY);
-        moveY            = glm::cross(normal, moveX);
-      }
-
-      moveX_[i] = moveX;
-      moveY_[i] = moveY;
-      angle_[i] = dist(gen) * 2 * PI - PI;
-
-      velocity_[i] = 1.0F + dist(gen) * 40;
-
-      ++i;
-    }
-  }
-
-  void writeToShapesBeforeRender(scene::ShapeGroup &shapeGroup) const
-  {
-    const float timeSec = std::chrono::duration<float>(std::chrono::steady_clock::now() - startedAt_).count();
-
-    for (std::size_t i = 0; scene::Shape &shape : shapeGroup.shapes)
-    {
-      const float velocity = velocity_[i];
-      const float angle    = angle_[i] + timeSec * velocity;
-      const float s        = std::sin(angle);
-      const float c        = std::cos(angle);
-
-      glm::vec3 pos   = positions_[i];
-      glm::vec3 moveX = moveX_[i];
-      glm::vec3 moveY = moveY_[i];
-
-      const glm::vec3 result = pos + moveX * c * R + moveY * s * R;
-
-      shape.offset = result;
-
-      ++i;
-    }
-  }
+  void writeToShapesBeforeRender(scene::ShapeGroup &shapeGroup) const;
 };
 
 world::World_01::World_01()
@@ -111,4 +52,67 @@ void world::World_01::initShapes(scene::ShapeGroup &atoms, const std::vector<sce
 void world::World_01::writeToShapesBeforeRender(scene::ShapeGroup &shapeGroup)
 {
   impl_->writeToShapesBeforeRender(shapeGroup);
+}
+
+void world::World_01::Impl::initShapes(const scene::ShapeGroup &atoms, const std::vector<scene::MaterialParams> &materials)
+{
+  std::random_device             rd;
+  std::mt19937                   gen(rd());
+  std::uniform_real_distribution dist(0.0F, 1.0F);
+
+  const std::size_t atomsCount = atoms.shapes.size();
+
+  positions_.resize(atomsCount);
+  moveX_.resize(atomsCount);
+  moveY_.resize(atomsCount);
+  angle_.resize(atomsCount);
+  atoms_.resize(atomsCount);
+  velocity_.resize(atomsCount);
+
+  for (std::size_t i = 0; const scene::Shape &shape : atoms.shapes)
+  {
+    const scene::MaterialParams material = materials[shape.materialIndex];
+
+    atoms_[i]     = material.atom;
+    positions_[i] = shape.offset;
+
+    glm::vec3 moveX = normalize(glm::vec3(dist(gen) - 0.5F, dist(gen) - 0.5F, dist(gen) - 0.5F));
+    glm::vec3 moveY = normalize(glm::vec3(dist(gen) - 0.5F, dist(gen) - 0.5F, dist(gen) - 0.5F));
+
+    {
+      glm::vec3 normal = glm::cross(moveX, moveY);
+      moveY            = glm::cross(normal, moveX);
+    }
+
+    moveX_[i] = moveX;
+    moveY_[i] = moveY;
+    angle_[i] = dist(gen) * 2 * PI - PI;
+
+    velocity_[i] = 1.0F + dist(gen) * 40;
+
+    ++i;
+  }
+}
+
+void world::World_01::Impl::writeToShapesBeforeRender(scene::ShapeGroup &shapeGroup) const
+{
+  const float timeSec = std::chrono::duration<float>(std::chrono::steady_clock::now() - startedAt_).count();
+
+  for (std::size_t i = 0; scene::Shape &shape : shapeGroup.shapes)
+  {
+    const float velocity = velocity_[i];
+    const float angle    = angle_[i] + timeSec * velocity;
+    const float s        = std::sin(angle);
+    const float c        = std::cos(angle);
+
+    glm::vec3 pos   = positions_[i];
+    glm::vec3 moveX = moveX_[i];
+    glm::vec3 moveY = moveY_[i];
+
+    const glm::vec3 result = pos + moveX * c * R + moveY * s * R;
+
+    shape.offset = result;
+
+    ++i;
+  }
 }
